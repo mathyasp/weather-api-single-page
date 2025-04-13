bdbda572d0b6b44ad8d5ee7636da480ac6ba4cc4
mathyasp <mathyastp@gmail.com>
2025-04-13 11:57:04 -0400

    Controlled input form

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Weather.css'
 
 function Weather() {
+  const [zipCode, setZipCode] = useState('')
+
+  const handleChange = (e) => {
+    setZipCode(e.target.value)
+  }
+
   return (
     <div className="weather-container">
       <h1>Weather Forecast</h1>
+      <form>
+        <input
+          type="text"
+          value={zipCode}
+          onChange={handleChange}
+          placeholder="Enter zip code"
+          pattern="[0-9]*"
+          maxLength="5"
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+      <p>Current zip code: {zipCode}</p>
     </div>
   )
 }
