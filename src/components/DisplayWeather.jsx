2305d6b202f7def0810ad6a5430e5c99e929db37
mathyasp <mathyastp@gmail.com>
2025-04-13 13:38:26 -0400

    Stretch: Add pressure and icon

--- src/components/DisplayWeather.jsx
+++ src/components/DisplayWeather.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import './DisplayWeather.css'
 
 function DisplayWeather({weatherData, units}) {
-
   return (
-    (
-      <div className="weather-info">
+    <div className="weather-info">
+      <div className="weather-heading">
         <h2>{weatherData.name}</h2>
-        <p className="temperature">{weatherData.main.temp}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F' : 'K'}</p>
-        <p className="description">{weatherData.weather[0].description}</p>
-        <p>Feels like: {weatherData.main.feels_like}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F' : 'K'}</p>
-        <p>Humidity: {weatherData.main.humidity}%</p>
-        <p>Wind: {weatherData.wind.speed} {units === 'imperial' ? 'mph' : 'm/s'}</p>
+        <img 
+          src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+          alt={weatherData.weather[0].description}
+          className="weather-icon"
+        />
       </div>
-    )
+
+      <p className="temperature">{weatherData.main.temp}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F' : 'K'}</p>
+      <p className="description">{weatherData.weather[0].description}</p>
+      <p>Feels like: {weatherData.main.feels_like}°{units === 'metric' ? 'C' : units === 'imperial' ? 'F' : 'K'}</p>
+      <p>Humidity: {weatherData.main.humidity}%</p>
+      <p>Wind: {weatherData.wind.speed} {units === 'imperial' ? 'mph' : 'm/s'}</p>
+      <p>Pressure: {weatherData.main.pressure} hPa</p>
+    </div>
   )
 }
 
